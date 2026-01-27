f1b2b9adfbf6e4d9991a7009d8421d1fc5327d49
Sakshi Trambadia <sakshitrambadia138@gmail.com>
2026-01-27 12:57:09 +0530

    first commit

--- learning26/urls.py
+++ learning26/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("test/",views.test),
+    path("about/",views.AboutUs),
+    path("contact/",views.contactUs),
+    path("",views.home),
+    path("movies/",views.movies),
+    path("shows/",views.shows),
+    path("news/",views.news)
+    
 ]
---
+++ learning26/views.py
@@ -0,0 +1,27 @@
+from  django.http import HttpResponse
+from django.shortcuts import render
+
+#specifi url
+def test(request):
+    return HttpResponse("Hello")
+
+# def AboutUs(request):
+#     return HttpResponse("About")
+
+def AboutUs(request):
+    return render(request,"AboutUs.html")
+
+def contactUs(request):
+    return render(request,"contactus.html")
+
+def home(request):
+    return render(request,"home.html")
+
+def movies(request):
+    return render(request,"movies.html")
+
+def shows(request):
+    return render(request,"shows.html")
+
+def news(request):
+    return render(request,"news.html")
